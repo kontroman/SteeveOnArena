50401fdf6c5511baf492327da36ab05c375fd5b3
sqWeeky <ilya.mil99@gmail.com>
2024-10-13 22:02:21 +0500

    FixedBugAnimationItem

--- Assets/Scripts/Item/Animation.cs
+++ Assets/Scripts/Item/Animation.cs
@@ -1,17 +1,17 @@
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UIElements;
 
+[ExecuteInEditMode]
 public class Animation : MonoBehaviour
 {
-    [SerializeField] private Vector3 _position;
+    [SerializeField] private Vector3 _positionInput;
     [SerializeField] private Vector3 _rotation;
     [SerializeField] private float _duration;
     [SerializeField] private int _repeats;
 
     private void Start()
     {
-        transform.DOMove(_position, _duration).SetLoops(_repeats, LoopType.Yoyo).SetEase(Ease.Linear);
+        transform.DOMove(transform.position + _positionInput, _duration).SetLoops(_repeats, LoopType.Yoyo).SetEase(Ease.Linear);
         transform.DORotate(_rotation, _duration).SetLoops(_repeats, LoopType.Restart).SetEase(Ease.Linear);
     }
 }
