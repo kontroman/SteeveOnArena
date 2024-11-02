1aac35c1140e6363a005e7b47ab6b47bd4e2d0ab
sqWeeky <ilya.mil99@gmail.com>
2024-11-02 21:59:44 +0500

    ImprovedSystemDrop

--- Assets/Scripts/DropableObjects/Dropable.cs
+++ Assets/Scripts/DropableObjects/Dropable.cs
@@ -5,41 +5,65 @@ namespace Devotion.Drop
 {
     public class Dropable : MonoBehaviour
     {
-        [SerializeField] private List<Items.Item> _items;
-        [SerializeField] private List<int> _chance;
+        [Header("List drops")]
+        [SerializeField] private List<Drop> _drops;
 
-        private Dictionary<Items.Item, int> _dictionaryItems = new();
-        private int _maxChanceDrop = 51;
+        [Header("Drop only or more ")]
+        [SerializeField] private bool _isOneDrop;
+
+        private int _maxChanceDrop = 101;
         private int _minChanceDrop = 0;
         private int _currentChanceDrop;
-
-        private void Awake()
-        {
-            for (int i = 0; i < _items.Count && i < _chance.Count; i++)
-            {
-                _dictionaryItems.Add(_items[i], _chance[i]);
-            }
-        }
+        private List<Drop> _currentDrops;
 
         private void Start()
         {
+            _currentDrops = new List<Drop>();
             _currentChanceDrop = Random.Range(_minChanceDrop, _maxChanceDrop);
-            Debug.Log(_currentChanceDrop);
+            Debug.Log(gameObject.name + "  " + _currentChanceDrop);
         }
 
         private void OnDestroy()
         {
-            Drop();
+            if (_isOneDrop)
+                DropVers1();
+            else
+                DropVers2();
         }
 
-        public void Drop()
+        // only one item drops
+        private void DropVers1()
         {
-            foreach (var item in _dictionaryItems)
+            for (int i = 0; i < _drops.Count; i++)
             {
-                if (item.Value >= _currentChanceDrop)
+                if (_drops[i].LowerBound <= _currentChanceDrop && _currentChanceDrop <= _drops[i].UpperBound)
                 {
-                    Debug.Log("drop item");
-                    var obj = Instantiate(item.Key.Prefab);
+                    var obj = Instantiate(_drops[i].Item.Prefab);
+                    obj.transform.position = transform.position;
+                    break;
+                }
+            }
+        }
+
+        // method allows to drop an item if the drop chance falls into several ranges
+        private void DropVers2()
+        {
+            for (int i = 0; i < _drops.Count; i++)
+            {
+                if (_drops[i].LowerBound <= _currentChanceDrop && _currentChanceDrop <= _drops[i].UpperBound)
+                {
+                    _currentDrops.Add(_drops[i]);
+                }
+            }
+
+            _currentChanceDrop = Random.Range(0, _currentDrops.Count + 1);
+            Debug.LogError(gameObject.name + "  " + _currentChanceDrop);
+
+            for (int i = 0; i < _currentDrops.Count; i++)
+            {
+                if (i == _currentChanceDrop)
+                {
+                    var obj = Instantiate(_currentDrops[i].Item.Prefab);
                     obj.transform.position = transform.position;
                     break;
                 }
