4f12193107965c1db90992948238f168fd143470
sqWeeky <ilya.mil99@gmail.com>
2025-02-12 22:08:15 +0500

    Revert "FixedBug"
    This reverts commit c8caa328746ef639fbe0d82f5fc71f82d7551bcf.

--- Assets/Scripts/Managers/InteractionManager.cs
+++ Assets/Scripts/Managers/InteractionManager.cs
@@ -17,10 +17,7 @@ namespace Devotion.Managers
             UpdateClosestObject();
 
             if (_currentInteractable != null && Input.GetKeyDown(KeyCode.E))
-            {
-                _currentInteractable?.HideInteractionPrompt();
                 _currentInteractable.ExecuteCommand();
-            }    
         }
 
         public void RegisterObject(InteractableObject interactable)
