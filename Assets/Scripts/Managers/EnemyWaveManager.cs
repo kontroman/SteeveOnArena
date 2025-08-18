89693f2f2462b0e379cdfe8bb8bcda40bcd69168
Kontroman <ckontro@gmail.com>
2025-08-18 19:10:52 +0600

    backup

---
+++ Assets/Scripts/Managers/EnemyWaveManager.cs
@@ -0,0 +1,27 @@
+using Devotion.SDK.Managers;
+using MineArena.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MineArena.Managers
+{
+    public class EnemyWaveManager : BaseManager
+    {
+        public void ActivateManager()
+        {
+
+        }
+
+        private void StartSpawnEnemies()
+        {
+
+        }
+
+        private Mob SpawnNextEnemy()
+        {
+
+            return null;
+        }
+    }
+}
--- Assets/Scripts/Windows/SelectLevelWindow/LevelDescription.cs
+++ Assets/Scripts/Windows/SelectLevelWindow/LevelDescription.cs
@@ -1,6 +1,7 @@
 using MineArena.Levels;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MineArena.Windows.SelectLevel
 {
@@ -8,17 +9,42 @@ namespace MineArena.Windows.SelectLevel
     {
         [SerializeField] private TextMeshProUGUI difficultyText;
 
+        [SerializeField] private Button startButton;
+
         [SerializeField] private Transform availableTransform;
         [SerializeField] private Transform rewardTransform;
 
-        private LevelConfig config;
+        [SerializeField] private GameObject resourcePrefab;
+
+        private LevelConfig _config;
 
         public void Initialize(LevelConfig config)
         {
-            this.config = config;
+            this._config = config;
+
+            SetupUI();
         }
 
         private void SetupUI()
+        {
+            difficultyText.text = _config.Difficulty.ToString();
+
+            foreach (var item in _config.AvailableResources)
+            {
+                var resource = Instantiate(resourcePrefab, availableTransform).GetComponent<Image>();
+                resource.sprite = item.Icon;
+            }
+
+            foreach (var item in _config.RewardResources)
+            {
+                var resource = Instantiate(resourcePrefab, rewardTransform).GetComponent<Image>();
+                resource.sprite = item.Icon;
+            }
+
+            startButton.onClick.AddListener(StartLevel);
+        }
+
+        private void StartLevel()
         {
 
         }
