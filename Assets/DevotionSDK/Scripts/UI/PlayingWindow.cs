f6d017b5dba2d4f92ae51508647306b2402667cf
Kontroman <ckontro@gmail.com>
2025-09-04 15:09:11 +0600

    Update PlayingWindow.cs

--- Assets/DevotionSDK/Scripts/UI/PlayingWindow.cs
+++ Assets/DevotionSDK/Scripts/UI/PlayingWindow.cs
@@ -1,9 +1,10 @@
 using Devotion.SDK.Base;
+using Devotion.SDK.Controllers;
 
 namespace Devotion.SDK.UI
 {
     public class PlayingWindow : BaseWindow
     {
-
+        public void OnAchievmentButtonClick() => GameRoot.UIManager.ShowWindow<AchievemntQuest>();
     }
 }
